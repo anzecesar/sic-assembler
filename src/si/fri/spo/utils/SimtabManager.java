43a817e1549003e207febdb890b5a35f1d44a4d0
obscenity <ces@kiberpipa.org>
2009-05-10 14:12:18 +0200

    Dodal SimtabManager, podoben kot Mnetabmanager, restrukturiral Assembler

---
+++ src/si/fri/spo/utils/SimtabManager.java
@@ -0,0 +1,38 @@
+package si.fri.spo.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import si.fri.spo.exceptions.NapakaPriPrevajanju;
+
+public class SimtabManager {
+	private static SimtabManager m_instance;
+	
+	Map<String, Integer> simTab;
+	
+	private SimtabManager() {
+		simTab = new HashMap<String, Integer>();
+	}
+	
+	public static SimtabManager getInstance() {
+		if(m_instance == null)
+			m_instance = new SimtabManager();
+		
+		return m_instance;
+	}
+	
+	/**
+	 * Doda labelo v tabelo simtab
+	 * @param labela
+	 * @param lokSt
+	 * @throws NapakaPriPrevajanju Napaka, če je dodana labela že vsebovana.
+	 */
+	public void dodajLabelo(String labela, Integer lokSt) throws NapakaPriPrevajanju {
+		if(simTab.containsKey(labela)) {
+			throw new NapakaPriPrevajanju("Napaka: Labela " + labela + " je definirana dvakrat!");
+		}
+		
+		simTab.put(labela, lokSt);
+	}
+
+}
